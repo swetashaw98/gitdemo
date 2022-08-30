8d5bc2c6cb81d187bdf54120ce9c58124ef91610
Sweta Shaw <sweta.shaw98@gmail.com>
2022-08-30 07:42:49 +0530

    added comment

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public void sum(){
     public static void main(String[] args){
         init a=1;
         init b=2;
-        system.out.println(a+b);
+        system.out.println("the sum is: "+(a+b));
 
     }
 }
